f94447f695980d7af48952406ee440b640fb3621
Johnny Reveles <Johnny Reveles@LAPTOP-PO4S021M>
2021-02-24 19:44:35 -0600

    Imprimir nombre

---
+++ src/gitgrupoa66659/GitGrupoA66659.java
@@ -0,0 +1,22 @@
+/*
+ UNIVERSIDAD AUTÓNOMA DE CAMPECHE
+ Facultad de Ingeniería
+ Ingeniería en Sistemas Computacionales
+ */
+package gitgrupoa66659;
+
+/**
+ *
+ * @author Johnny Reveles
+ */
+public class GitGrupoA66659 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Johnny");
+    }
+    
+}
